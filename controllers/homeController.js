e3842f9a2bdf3761ae7a87060ae3a08b52d3a6a8
Jason1420 <hai.truong1420@gmail.com>
2023-09-13 16:50:47 +0700

    CRUD - C

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,5 @@
 import db from '../models/index'
-
+import CRUDService from '../services/CRUDService';
 const getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -10,12 +10,22 @@ const getHomePage = async (req, res) => {
         console.log(error);
     }
 }
+
 const getAboutPage = (req, res) => {
     return res.render('test/aboutPage.ejs')
 }
 
+const getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+const postCRUD = async (req, res) => {
+    const message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server')
+}
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
-
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
